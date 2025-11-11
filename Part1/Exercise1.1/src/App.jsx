9787d762d5604d42aa43413ba384cc6ec97f003a
Subod4 <subod4.badal@gmail.com>
2025-11-11 18:32:20 +0545

    event Handling

--- Part1/Exercise1.1/src/App.jsx
+++ Part1/Exercise1.1/src/App.jsx
@@ -1,12 +1,30 @@
 import {useState} from 'react'
+const Display = (props) =>{
+  return(
+    <div>{props.counter}</div>
+  )
+}
+const Button = (props) =>{
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 const App = () =>{
   const [counter, setCounter] = useState(0)
-  setTimeout(
-    ()=> setCounter(counter + 1),1000 
-  )
+  const increse = () => setCounter(counter+1)
+  const zero = () => setCounter(0)
+  const handleClick = () =>{
+    console.log('click')
+  }
   console.log('rendering...', counter)
   return(
-      <div>{counter}</div>
+      <>
+        <Display counter = {counter}/>
+        <Button onClick = {increse} text = 'plus'/>
+        <Button onClick = {zero} text = 'zero'/>
+      </>
   )
 }
 export default App
