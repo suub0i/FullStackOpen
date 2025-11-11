252cbf46db68f412cef600e78a3ce0063b9af11f
Subod4 <subod4.badal@gmail.com>
2025-11-11 18:12:18 +0545

    re-rendering

--- Part1/Exercise1.1/src/App.jsx
+++ Part1/Exercise1.1/src/App.jsx
@@ -1,26 +1,12 @@
-const Hello = ({name, age}) =>{
-  const bornYear =  () => new Date().getFullYear() - age
+import {useState} from 'react'
+const App = () =>{
+  const [counter, setCounter] = useState(0)
+  setTimeout(
+    ()=> setCounter(counter + 1),1000 
+  )
+  console.log('rendering...', counter)
   return(
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-
-      </p>
-      <p>So you were born in {bornYear()}</p>
-    </div>
+      <div>{counter}</div>
   )
 }
-
-const App = () => {
-  const name = 'Ram'
-  const age = 10
-
-  return(
-    <div>
-      <h1>Greeting</h1>
-      <Hello name = 'Maya' age = {30 + 10}/>
-      <Hello name = {name} age = {age}/>
-    </div>
-  )
-} 
-export default App;
+export default App
